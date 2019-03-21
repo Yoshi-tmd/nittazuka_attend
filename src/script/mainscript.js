78b896949eb57b6d21c05f200de1e572ffaa0ce5
ythomePC <tmd_bkcoffee@outlook.jp>
2019-03-21 19:34:30 +0900

    人数と金額を算出するscriptを実装；応用クラスページを新規作成

---
+++ src/script/mainscript.js
@@ -0,0 +1,61 @@
+// mainscript.js
+
+// 今日の日付を表示
+window.onload = function(){
+    // 今日の日時を取得
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+
+    // 月と日を2桁表示にする処理
+    var toTwoDigits = function(num, digit){
+        num += '';
+        if (num.length < digit){
+            num = '0' + num;
+        }
+        return num;
+    }
+
+    var yyyy = toTwoDigits(year, 4);
+    var mm = toTwoDigits(month, 2);
+    var dd = toTwoDigits(day, 2);
+    var ymd = yyyy + "-" + mm + "-" + dd;
+
+    document.getElementById("club-date").value = ymd;
+}
+
+const course = document.getElementById('formarea');
+console.log(course.dataset.course);
+let unitPrice;
+
+if(course.dataset.course == 'basic'){
+    unitPrice = 200;
+} else {
+    unitPrice = 300;
+}
+console.log(unitPrice);
+
+function showValue(){
+    let menNum = 8;
+    let count = 0;
+
+    for (let i = 1; i <= menNum; i++){
+        let str = 'radio0' + i + '-attend';
+        console.log(str);
+        
+        let val  = document.getElementById(str);
+        console.log(val);
+        if(val.checked){
+            count++;
+        }
+    }
+    console.log(count);
+    document.getElementById('resultNum').innerHTML = count;
+
+    total = count * unitPrice;
+    if(total){
+        document.getElementById('resultAmount').innerHTML = total;
+    }
+}
+
